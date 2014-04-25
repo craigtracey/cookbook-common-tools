142febdc6d455937c62031746e487666011aa1d6
Craig Tracey <craigtracey@gmail.com>
2014-04-25 18:51:04 -0400

    Update metadata
    Use appropriate fields

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*
-name        'common-tools'
-author      'Craig Tracey <craigtacey@gmail.com>'
-version     '0.0.1'
+name              'common-tools'
+maintainer        'Craig Tracey'
+maintainer_email  'craigtracey@gmail.com'
+license           'Apache 2.0'
+version           '0.0.1'
 
-depends     's3cmd', '~ 0.2.0'
+depends           's3cmd', '~> 0.2.0'
